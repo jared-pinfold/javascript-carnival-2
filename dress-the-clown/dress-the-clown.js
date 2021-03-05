06ad5529d627c269ca83c01ec307e7b1ec29c1e1
Jared <j.pinfold@gmail.com>
2021-03-05 13:50:35 +1300

    copied across everything from the original broken repo

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -2,4 +2,73 @@
  // JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
+// Declare global variables
+
+var headIndex = 0
+var bodyIndex = 0
+var shoesIndex = 0
+var bodyParts = ["shoes" , "body", "head"]
+var relevantIndex = [shoesIndex, bodyIndex, headIndex]
+var bodyPartIndex = 2
+
+// detect key presses
+
+document.addEventListener('keydown', function(e) {
+  switch (e.keyCode) {
+    case 37:
+      change("down"); // left
+      break;
+    case 38:
+      changeBodyPartIndex("up"); // up
+      break;
+    case 39:
+      change("up"); // right
+      break;
+    case 40:
+      changeBodyPartIndex("down"); // down
+      break;
+  }
+});
+
+// Change the body part index
+
+function changeBodyPartIndex (direction) {
+  if (direction == "up") {
+    if (bodyPartIndex === 2) {
+      bodyPartIndex = 0
+    } else {
+      bodyPartIndex ++
+    }
+  } else if (direction == "down") {
+    if (bodyPartIndex === 0) {
+      bodyPartIndex = 2
+    } else {
+      bodyPartIndex --
+    }
+  }
+  let image = document.getElementById("arrow")
+  image.src = "images/arrow" + bodyPartIndex + ".png"
+}
+
+// change any body part
+
+function change (direction) {
+  if (direction == "up") {
+    if (relevantIndex[bodyPartIndex] == 5) {
+      relevantIndex[bodyPartIndex] = 0
+    } else {
+      relevantIndex[bodyPartIndex] ++
+    }
+  } else if (direction == "down") {
+    if (relevantIndex[bodyPartIndex] == 0) {
+      relevantIndex[bodyPartIndex] = 5
+    } else {
+      relevantIndex[bodyPartIndex] --
+    }
+  }
+  let image = document.getElementById(bodyParts[bodyPartIndex])
+  image.src = "images/" + bodyParts[bodyPartIndex] + relevantIndex[bodyPartIndex] + ".png"
+}
+
 console.log("Dress The Clown!")
+
--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,49 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+
+// Create a variable to house the status of each unicorn
+var unicorn1 = {
+  inflationStatus: 0,
+  idTag: "unicorn1",
+}
+
+var unicorn2 = {
+  inflationStatus: 0,
+  idTag: "unicorn2",
+}
+
+var unicorn3 = {
+  inflationStatus: 0,
+  idTag: "unicorn3",
+}
+
+var images = [
+  "./images/unicorn-0.png",
+  "./images/unicorn-1.png",
+  "./images/unicorn-2.png",
+  "./images/unicorn-3.png"
+]
+
+// var inflateAudio = document.getElementById("inflate");
+// var deflateAudio = document.getElementById("deflate");
+function inflateAudio () {
+  document.getElementById("inflate").play()
+}
+
+function deflateAudio () {
+  document.getElementById("deflate").play()
+}
+
+function inflate(unicornId) {
+  // check status of unicorn - if at 3, do nothing, otherwise switch to next photo and increment status
+  if (unicornId.inflationStatus < 3) {
+    unicornId.inflationStatus ++
+    document.getElementById(unicornId.idTag).src = images[unicornId.inflationStatus]
+    inflateAudio()
+  } else {
+    unicornId.inflationStatus = 0
+    document.getElementById(unicornId.idTag).src = images[unicornId.inflationStatus]
+    deflateAudio()
+  }
+}
